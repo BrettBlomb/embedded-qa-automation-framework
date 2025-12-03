5beb94fd2d3edbd5637f69df51630d42853915dc
Brett Blomberg <49074821+BrettBlomb@users.noreply.github.com>
2025-12-03 13:50:58 -0800

    Update mock_coap_server.py

--- mock_servers/mock_coap_server.py
+++ mock_servers/mock_coap_server.py
@@ -1,1 +1,18 @@
+# mock_coap_server.py
+import asyncio
+from aiocoap import Message, Context, resource
 
+class StatusResource(resource.Resource):
+    async def render_get(self, request):
+        payload = b'{"status": "ok"}'
+        return Message(payload=payload)
+
+def main():
+    root = resource.Site()
+    root.add_resource(['status'], StatusResource())
+
+    asyncio.Task(Context.create_server_context(root))
+    asyncio.get_event_loop().run_forever()
+
+if __name__ == "__main__":
+    main()
